9102276982913568a508bfa9b174a35b6ea02c55
Jatin Kushwaha <jatinkushwaha995@gmail.com>
2025-07-13 15:34:07 +0530

    forth version

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // define the mongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/resturant'
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 //Set up MongoDB connection
 mongoose.connect(mongoURL , {
